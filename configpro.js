e2608272694fe8008108f2d268691b855392e847
Your Name <you@example.com>
2020-01-09 15:41:12 +0700

    f

---
+++ configpro.js
@@ -0,0 +1,58 @@
+[
+	{
+		"type": "p",
+		"version": "1.1",
+		"supportTypes": ["movie", "tv"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/p.general.js"
+	},{
+		"type": "p",
+		"version": "1.1",
+		"supportTypes": ["movie", "tv"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/p.generalone.js"
+	},{
+		"type": "p",
+		"version": "1.1",
+		"supportTypes": ["movie", "tv"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/p.5movies.js"
+	},{
+		"type": "p",
+		"version": "1.0",
+		"supportTypes": ["tv"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/p.watchepisode4.js"
+	},{
+		"type": "h",
+		"version": "1.0",
+		"supportTypes": ["mixdrop.co"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/h.mixdrop.js"
+	},
+	{
+		"type": "h",
+		"version": "1.0",
+		"supportTypes": ["onlystream.tv"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/h.onlystream.js"
+	},
+	{
+		"type": "h",
+		"version": "1.0",
+		"supportTypes": ["clipwatching.com"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/h.clipwatching.js"
+	},
+	{
+		"type": "h",
+		"version": "1.0",
+		"supportTypes": ["uptostream.com", "uptobox.com"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/h.uptostream.js"
+	},
+	{
+		"type": "h",
+		"version": "1.0",
+		"supportTypes": ["gounlimited.to"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/h.gounlimited.js"
+	},
+	{
+		"type": "h",
+		"version": "1.0",
+		"supportTypes": ["vidoza.net"],
+		"url": "https://raw.githubusercontent.com/drinkcocacola/orfanta/master/build/h.vidoza.js"
+	}
+]
